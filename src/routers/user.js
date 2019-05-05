fb7b924bc1dea6788cd9395eefc61cdd1b92dc36
josemp10 <josemp10@gmail.com>
2019-05-05 06:04:13 -0700

    Adding middleware and logouts

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -31,14 +32,30 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout/', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token )
+        await req.user.save()
 
-    try{
-        const users = await User.find({})
-        res.send (users)
+        res.send()
     } catch (e) {
         res.status(500).send()
     }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+
+    }
+})
+
+router.get('/users/me', auth ,async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
