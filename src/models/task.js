c3863be7b837de3269138b475fc8e98f98019e83
josemp10 <josemp10@gmail.com>
2019-05-06 08:05:19 -0700

    Sorting and paging

--- src/models/task.js
+++ src/models/task.js
@@ -1,8 +1,7 @@
 const mongoose = require ('mongoose')
 const validator = require('validator')
 
-const Task = mongoose.model('Task', {
-    description: {
+const taskSchema = new mongoose.Schema({description: {
         type: String,
         required: true,
         trim:true
@@ -16,6 +15,10 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User'
     }
+}, {
+    timestamps: true
 })
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
